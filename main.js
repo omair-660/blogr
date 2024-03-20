893ee508d439ccc2a24100147aa1e5bfb8ddea48
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-03-20 17:14:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let hum = document.getElementById("hum");
+let linkss = document.getElementById("linkss");
+
+hum.onclick = function () {
+
+  hum.classList.toggle("activee");
+  linkss.classList.toggle("humbrg");
+
+}
